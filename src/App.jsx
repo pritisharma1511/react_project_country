1dfe34fa0dc4cafb1b2a8290e94c3d2f26d4a706
pritisharma1511 <ps0130815@gmail.com>
2025-09-21 10:54:03 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Country } from "./pages/Country";
+import { Contact } from "./pages/Contact";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/about",
+        element: <About />
+    },
+    {
+        path: "/country",
+        element: <Country />
+    },
+    {
+        path: "/contact",
+        element: <Contact />
+    }
+]);
+
 const App = () => {
-    return <h1>App Component</h1>;
+    return <RouterProvider router={router}> </RouterProvider>;
 }
+
+
 export default App;
