d683671d1e91217ccff4319e7e2530841bd95475
pritisharma1511 <ps0130815@gmail.com>
2025-09-21 13:35:21 +0530

    section created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,12 @@
-import { FaLongArrowAltRight } from "react-icons/fa";
+
+import { HeroSection } from "../components/UI/HeroSection";
+import { About } from "./About";
 
 export const Home = () => {
-    return (
-        <main className="hero-section main">
-            <div className="container grid grid-two-cols">
-                <div className="hero-context">
-                    <h1 className=" heading-xl">
-                    Explore the World,One country at a time
-                    </h1>
-                    <p className="paragraph">
-                    Discover the history,culture and beauty of every nation .Sort ,search and filter through detailed information about countries around the globe.
-                    </p>
-                    <button className="btn btn-darken btn-inline bg-white-box">Start exploring <FaLongArrowAltRight /></button>
-                </div>
-             <div className="hero-image">
-                    <img src="./images/world.png" alt="world beauty" className="banner-image"/>
-                </div>
-            </div>
-        </main>
-    );
+  return (
+    <>
+      <HeroSection />
+      <About />
+    </>
+  );
 };
