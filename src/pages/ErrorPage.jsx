a8d82cb7c933b4928540dd2cfa3b7ba979f19a6b
pritisharma1511 <ps0130815@gmail.com>
2025-09-21 11:51:07 +0530

    DONE

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,16 @@
-import { useRouteError } from "react-router-dom";
+import { NavLink, useRouteError } from "react-router-dom";
 
 export const ErrorPage = () => {
-    const error = useRouteError();
-    console.error(error);
-    return <h1>Error Page</h1>;
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div>
+      <h1>Oops! An error occurred.</h1>
+      {error && <p>{error.data}</p>}
+      <NavLink to="/">
+        <button> Go Home </button>
+      </NavLink>
+    </div>
+  );
 };
