07f8e9c87d6fa8b324223d11adb4247413401bb7
pritisharma1511 <ps0130815@gmail.com>
2025-09-21 12:36:52 +0530

    home page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,22 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+
 export const Home = () => {
-    return <h1>Home Page</h1>;
+    return (
+        <main className="hero-section main">
+            <div className="container grid grid-two-cols">
+                <div className="hero-context">
+                    <h1 className=" heading-xl">
+                    Explore the World,One country at a time
+                    </h1>
+                    <p className="paragraph">
+                    Discover the history,culture and beauty of every nation .Sort ,search and filter through detailed information about countries around the globe.
+                    </p>
+                    <button className="btn btn-darken btn-inline bg-white-box">Start exploring <FaLongArrowAltRight /></button>
+                </div>
+             <div className="hero-image">
+                    <img src="./images/world.png" alt="world beauty" className="banner-image"/>
+                </div>
+            </div>
+        </main>
+    );
 };
