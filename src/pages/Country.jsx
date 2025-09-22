57e30829b7ad0830ffe84f9ba52bbceabfaa3c34
pritisharma1511 <ps0130815@gmail.com>
2025-09-22 12:00:00 +0530

    loader file created

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,16 +1,18 @@
-import {  useEffect, useTransition } from "react";
+import {  useEffect, useTransition,useState } from "react";
 import { getCountryData } from "../api/postApi";
 
 export const Country = () => {
   const [isPending, startTransition] = useTransition();
+  const[countries, setCountries] = useState([]);
     
     
     useEffect(() => {
         startTransition(async() => {
             const res = await getCountryData();
             console.log(res);
+            setCountries(res.data);
         });
     }, []);
-    if (isPending) return <h1>Loading...</h1>
+    if (isPending) return <h1>Loading...</h1>;
     return <h1>Country Page</h1>;
 };
