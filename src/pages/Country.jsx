77fc05c4e7793dc69601c469c43d8a85e65f7d9e
pritisharma1511 <ps0130815@gmail.com>
2025-09-22 11:55:13 +0530

    country page added

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,16 @@
+import {  useEffect, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+
 export const Country = () => {
+  const [isPending, startTransition] = useTransition();
+    
+    
+    useEffect(() => {
+        startTransition(async() => {
+            const res = await getCountryData();
+            console.log(res);
+        });
+    }, []);
+    if (isPending) return <h1>Loading...</h1>
     return <h1>Country Page</h1>;
 };
